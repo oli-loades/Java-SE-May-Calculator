c638055f1dd0e1076507540d838711b37f4b3a30
oli <oli.loades@gmail.com>
2018-07-11 11:43:46 +0100

    refactored

--- src/main/java/com/qa/calculator/Calculator.java
+++ src/main/java/com/qa/calculator/Calculator.java
@@ -4,32 +4,28 @@ public class Calculator {
 	private Add add;
 	private Subtract subtract;
 	private Multiply multiply;
-	
+	private Divide divide;
+
 	public Calculator() {
 		add = new Add();
 		subtract = new Subtract();
 		multiply = new Multiply();
+		divide = new Divide();
 	}
-	
+
 	public int add(int num1, int num2) {
 		return add.add(num1, num2);
 	}
 
-	public int subtract(int num1,int num2) {
+	public int subtract(int num1, int num2) {
 		return subtract.subtract(num1, num2);
 	}
-	
+
 	public int multiply(int num1, int num2) {
 		return multiply.multiply(num1, num2);
 	}
 
 	public int divide(int num1, int num2) {
-		int result;
-			try {
-				result = num1/num2;
-			}catch(ArithmeticException ae) {
-				result = num1/(num2+1);
-			}
-		return result;
+		return divide.divide(num1, num2);
 	}
 }
